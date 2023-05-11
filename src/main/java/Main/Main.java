fb529d597d1c0a08c78d4ab420f507b2483760e6
JuanDiego <jmarmor2111@g.educaan.es>
2023-05-11 12:45:47 +0200

    Ejercicio p2

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+import application.FrameTotal;
+
+/**
+ *
+ * @author juandi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrameTotal f = new FrameTotal();
+        
+        f.setVisible(true);
+        
+        System.out.println(f);
+    }
+    
+}
